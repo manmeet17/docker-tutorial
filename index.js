8cb0817c53ba1b3d938a90d202349ead16ebafb0
Manmeet Tarun <meetutarun16@gmail.com>
2019-01-30 14:49:01 +0530

    Decker compose file

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT=3000;
 
+app.get('/me', (req, res)=>{
+    console.log("Called route");
+    res.send('<h2>Hellu</h2>');
+});
+
 app.get('/', (req,res)=>{
     res.sendFile(`${__dirname}/index.html`);
 });
